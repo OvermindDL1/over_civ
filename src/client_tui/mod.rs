5617968519ba2029ce5c7ae47de9d65d99dc1a69
OvermindDL1 <overminddl1@gmail.com>
2021-04-30 13:42:10 -0600

    More work on TUI input and screen setup

--- src/client_tui/mod.rs
+++ src/client_tui/mod.rs
@@ -2,6 +2,8 @@ mod tui_plugin;
 
 use crate::universal::exit::RequestExit;
 use bevy::app::{PluginGroupBuilder, ScheduleRunnerSettings};
+use bevy::input::keyboard::KeyboardInput;
+use bevy::input::ElementState;
 use bevy::prelude::*;
 use bevy::window::WindowCloseRequested;
 use std::time::Duration;
@@ -17,7 +19,14 @@ impl PluginGroup for ClientTuiPluginGroup {
 			.add(bevy::audio::AudioPlugin::default())
 			.add(bevy::gilrs::GilrsPlugin::default())
 			.add(bevy::app::ScheduleRunnerPlugin::default())
-			.add(tui_plugin::TuiRunnerPlugin::default())
+			.add(
+				tui_plugin::TuiRunnerPlugin::default()
+					.title(env!("CARGO_PKG_NAME"))
+					.start_in_raw_mode(true)
+					.start_with_mouse_captured(true)
+					.enable_alternate_screen(true)
+					.max_events_per_tick(128),
+			)
 			.add(ClientTuiPlugin)
 			.add(bevy::app::ScheduleRunnerPlugin::default());
 	}
@@ -28,7 +37,17 @@ impl Plugin for ClientTuiPlugin {
 		app.insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
 			1.0 / 20.0,
 		)))
-		.add_system(exit_on_window_close.system());
+		.add_system(exit_on_window_close.system())
+		.add_system(exit_on_escape.system());
+	}
+}
+
+fn exit_on_escape(mut keys: EventReader<KeyboardInput>, mut exit: EventWriter<RequestExit>) {
+	for key in keys.iter() {
+		if key.key_code == Some(KeyCode::Escape) && key.state == ElementState::Released {
+			trace!("escape pressed to request exit");
+			exit.send(RequestExit);
+		}
 	}
 }
 
